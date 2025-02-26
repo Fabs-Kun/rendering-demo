b53af4827008f0b0df2e97fc2f8a443331cab696
= <=>
2025-02-26 21:59:05 +0700

    Adding new code to test something inside about.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function aboutPage(){
+import { cookies } from "next/headers"
+
+export default async function aboutPage(){
+    const cookieStore = cookies();
+    const theme = (await cookieStore).get("theme");
+    console.log(theme)
     console.log("About server component")
     return <h1>About Page!{new Date().toLocaleTimeString()}</h1>
 }
