f3b662ff140be9194ed14ea2335973f22457ea54
= <=>
2025-02-26 23:06:49 +0700

    Adding new dir inside rendering-demo to test something.

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import { serverSideFunction } from "@/utils/server-utils";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+// import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ClientRoutePage() {
-    console.log("Client route rendered");
-    const result = serverSideFunction();
-  return (
-    <>
-        <h1>Client Route Page</h1>
-        <p>{result}</p>
-    </>
-  )
-}
+    const settings = {
+        dots: true,
+      };
+      return (
+        <div className="image-slider-container">
+          <Slider {...settings}>
+            <div>
+              <img src="https://picsum.photos/400/200" />
+            </div>
+            <div>
+              <img src="https://picsum.photos/400/200" />
+            </div>
+            <div>
+              <img src="https://picsum.photos/400/200" />
+            </div>
+            <div>
+              <img src="https://picsum.photos/400/200" />
+            </div>
+          </Slider>
+        </div>
+      );
+    }
