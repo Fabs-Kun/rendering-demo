b9aa29712825dfb5a8d20a9c624c8477641d50dd
= <=>
2025-02-26 22:23:14 +0700

    Adding suspense feature inside product-detail to test streaming rendering.

--- src/app/product-detail/page.tsx
+++ src/app/product-detail/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { Product } from "@/components/product"
 import { Reviews } from "@/components/reviews"
 
@@ -5,8 +6,12 @@ export default function ProductDetailPage() {
   return (
     <div>
         <h1>Product Detail Page</h1>
-        <Product />
-        <Reviews />
+        <Suspense fallback={<p>Loading Product Details...</p>}>
+            <Product />
+        </Suspense>
+        <Suspense fallback={<p>Loading Reviews...</p>}>
+            <Reviews />
+        </Suspense>
     </div>
   )
 }
